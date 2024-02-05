586aef794f5cef4714404892cfbd20e931504667
Svetlozar Georgiev <svetlozar.georgiev@evnisoft.com>
2024-02-06 01:34:12 +0200

    Arthouse

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -63,8 +63,8 @@ const SchedulerPage = () => {
   return (
     <Stack width="100%" maxWidth="87vw" gap={4}>
       <PageHeader
-        header="Клиенти"
-        subheader="Всичко за вашите клиенти на едно място"
+        header="График"
+        subheader="Менежирай лесно вашият график на всички клиенти"
         icon={<CalendarMonthOutlinedIcon sx={{ fontSize: "2.5rem" }} />}
       />
 
